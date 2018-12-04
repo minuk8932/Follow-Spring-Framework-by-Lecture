c51c33f7bfb90561e8561cfb6678e29c3427363a
minuk8932 <minuk8932@naver.com>
2018-12-04 17:43:27 +0900

    add Service(Customer) 181204

---
+++ helloMVC/src/service/CustomerSerivce.java
@@ -0,0 +1,34 @@
+package service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import model.Customer;
+
+public class CustomerSerivce {
+	private Map<String, Customer> customers;
+
+	public CustomerSerivce(Map<String, Customer> customers) {
+		customers = new HashMap<String, Customer>();
+		
+		addCustomer(new Customer("id001", "Alice", "alice.snut.ac.kr"));
+		addCustomer(new Customer("id002", "Bob", "bob.snut.ac.kr"));
+		addCustomer(new Customer("id003", "Charlie", "charlie.snut.ac.kr"));
+		addCustomer(new Customer("id004", "David", "david.snut.ac.kr"));
+		addCustomer(new Customer("id005", "Trudy", "trudy.snut.ac.kr"));
+	}
+		
+	public void addCustomer(Customer customer) {
+		customers.put(customer.getId(), customer);
+	}
+	
+	public Customer findCustomer(String id) {
+		if(id != null) {
+			return customers.get(id.toLowerCase());
+		}
+		
+		else {
+			return null;
+		}
+	}
+}
