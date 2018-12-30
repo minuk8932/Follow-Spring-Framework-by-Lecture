c92b4f0fd614331d240d8c5b38fb0230765fb40e
minuk8932 <minuk8932@naver.com>
2018-12-30 13:28:16 +0900

    add DAO Objects(Offer)

---
+++ helloDB/src/main/java/kr/ac/snut/spring/csemall/Offer.java
@@ -0,0 +1,15 @@
+package kr.ac.snut.spring.csemall;
+
+public class Offer {
+	private int id;
+	private String name;
+	private String email;
+	private String text;
+	
+	public Offer(int id, String name, String email, String text) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.text = text;
+	}
+}
