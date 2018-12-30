49822dce82e517d2af41eaa6f25c648fedba8d6e
minuk8932 <minuk8932@naver.com>
2018-12-30 13:48:15 +0900

    add method(getRowCount)

--- helloDB/src/main/java/kr/ac/snut/spring/csemall/OfferDAO.java
+++ helloDB/src/main/java/kr/ac/snut/spring/csemall/OfferDAO.java
@@ -1,5 +1,18 @@
 package kr.ac.snut.spring.csemall;
 
-public class OfferDAO {
+import javax.sql.DataSource;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class OfferDAO {
+	private JdbcTemplate jdbcTemplateObject;
+	
+	private void setDataSource(DataSource dataSource) {
+		this.jdbcTemplateObject = new JdbcTemplate(dataSource);
+	}
+	
+	public int getRowCount() {
+		String sqlStatement = "SELECT COUNT(*) from offers";
+		return jdbcTemplateObject.queryForObject(sqlStatement, Integer.class);
+	}
 }
