b1a171609d69e3945d51fa3216fbc4adda9b55cc
minuk8932 <minuk8932@naver.com>
2018-12-30 14:27:26 +0900

    add annotations(Component & Autowired)

--- helloDB/src/main/java/kr/ac/snut/spring/csemall/OfferDAO.java
+++ helloDB/src/main/java/kr/ac/snut/spring/csemall/OfferDAO.java
@@ -2,11 +2,15 @@ package kr.ac.snut.spring.csemall;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 
+@Component("offerDAO")
 public class OfferDAO {
 	private JdbcTemplate jdbcTemplateObject;
 	
+	@Autowired
 	public void setDataSource(DataSource dataSource) {
 		this.jdbcTemplateObject = new JdbcTemplate(dataSource);
 	}
