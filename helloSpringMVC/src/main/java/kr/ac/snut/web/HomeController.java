a1b312523cf3d0b8389311dc61cd1c9063869117
minuk8932 <minuk8932@naver.com>
2019-01-10 12:18:48 +0900

    init

--- helloSpringMVC/src/main/java/kr/ac/snut/web/HomeController.java
+++ helloSpringMVC/src/main/java/kr/ac/snut/web/HomeController.java
@@ -1,7 +1,5 @@
 package kr.ac.snut.web;
 
-import java.text.DateFormat;
-import java.util.Date;
 import java.util.Locale;
 
 import org.slf4j.Logger;
@@ -23,17 +21,10 @@ public class HomeController {
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+	public String showHome(Locale locale, Model model) {
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+		return "home";	// view logical name
 	}
 	
 }
