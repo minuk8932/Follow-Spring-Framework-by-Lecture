b4c19c7885c0557db2311adc8481ae123e44c957
minuk8932 <minuk8932@naver.com>
2019-01-20 14:31:25 +0900

    add check Validate input data

--- helloSpringMVC/src/main/java/kr/ac/snut/controller/OffersController.java
+++ helloSpringMVC/src/main/java/kr/ac/snut/controller/OffersController.java
@@ -7,6 +7,8 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.ac.snut.model.Offer;
@@ -36,7 +38,18 @@ public class OffersController {
 	}
 	
 	@RequestMapping("/docreate")
-	public String doCreate(Model model, @Valid Offer offer) {
+	public String doCreate(Model model, @Valid Offer offer, BindingResult result) {
+		if(result.hasErrors()) {
+			System.out.println("Form data does not Validate");
+			
+			List<ObjectError> errors = result.getAllErrors();
+			for(ObjectError error: errors) {
+				System.out.println(error.getDefaultMessage());
+			}
+			
+			return "createoffer";
+		}
+		
 		offersService.insert(offer);
 		
 		return "offercreated";
--- helloSpringMVC/src/main/java/kr/ac/snut/model/Offer.java
+++ helloSpringMVC/src/main/java/kr/ac/snut/model/Offer.java
@@ -1,9 +1,21 @@
 package kr.ac.snut.model;
 
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
 public class Offer {
 	private int id;
+	
+	@Size(min = 2, max = 50, message = "Name size must be between 2 and 50 chars")
 	private String name;
+	
+	@Email(message = "Please enter a valid email address")
+	@NotEmpty(message = "Email address cannot be empty")
 	private String email;
+	
+	@Size(min = 1, max = 1000, message = "Text size must be between 1 and 1000 chars")
 	private String text;
 	
 	public Offer() {}
